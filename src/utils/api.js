86f90d8cb7d3afe4e9bdbac66e68c7f0b2533b18
Aniket Kumar <97465559+Aniket2805@users.noreply.github.com>
2024-05-30 13:02:26 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://real-time-product-search.p.rapidapi.com";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "0f0dd2d684mshaf5ee0b7f7361f7p162f0fjsnad3e59b26f5f",
+    "X-RapidAPI-Key": "3fc742d33amsh2b3c1390930b288p1eb281jsn0c671a8dae7a",
     "X-RapidAPI-Host": "real-time-product-search.p.rapidapi.com",
   },
 };
